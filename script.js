93116d775ce33dd8ff52114493886c05b886bb3b
sujon12210 <160887695+sujon12210@users.noreply.github.com>
2026-01-05 14:29:40 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+function increase(){
+  count++;
+  document.getElementById('count').innerText = count;
+}
+
+function decrease(){
+  count--;
+  document.getElementById('count').innerText = count;
+}
